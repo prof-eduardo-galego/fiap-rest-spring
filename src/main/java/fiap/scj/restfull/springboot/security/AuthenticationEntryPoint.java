7b5717a7717759e37d3d577dad4bb2123d323648
Eduardo Galego <efgalego@ime.usp.br>
2018-04-19 15:25:44 -0300

    Add basic auth

---
+++ src/main/java/fiap/scj/restfull/springboot/security/AuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package fiap.scj.restfull.springboot.security;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+			throws IOException, ServletException {
+		response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		PrintWriter writer = response.getWriter();
+		writer.println("Não permitido - " + authEx.getMessage());
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		setRealmName("DeveloperStack");
+		super.afterPropertiesSet();
+	}
+
+}
---
+++ src/main/java/fiap/scj/restfull/springboot/security/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package fiap.scj.restfull.springboot.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic().authenticationEntryPoint(authEntryPoint);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("{noop}123").roles("USER");
+	}
+	
+}
