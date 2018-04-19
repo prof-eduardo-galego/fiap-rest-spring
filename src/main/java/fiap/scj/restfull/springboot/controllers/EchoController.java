0410e822effd21e0ff3e0078142d28668a26f7bd
Eduardo Galego <efgalego@ime.usp.br>
2018-04-19 16:37:41 -0300

    Add roles support

--- src/main/java/fiap/scj/restfull/springboot/controllers/EchoController.java
+++ src/main/java/fiap/scj/restfull/springboot/controllers/EchoController.java
@@ -1,5 +1,6 @@
 package fiap.scj.restfull.springboot.controllers;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import fiap.scj.restfull.springboot.Message;
 public class EchoController {
 
 	@GetMapping("/opa")
+	@Secured("ROLE_ADMIN")
 	public Message getMessage(@RequestParam(value="message", defaultValue="World") String message) {
 		return new Message(message);
 	}
--- src/main/java/fiap/scj/restfull/springboot/security/SpringSecurityConfig.java
+++ src/main/java/fiap/scj/restfull/springboot/security/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -27,7 +29,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}123").roles("USER");
+			.withUser("admin").password("{noop}123").roles("ADMIN")
+			.and()
+			.withUser("user").password("{noop}123").roles("USER");
 	}
 	
 }
